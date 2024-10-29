4e69e0105532cc6abe4309efc951b02a4584e689
Quoding <quoding@protonmail.ch>
2024-10-29 17:10:48 -0400

    hardcode arguments

--- train-wa.py
+++ train-wa.py
@@ -25,21 +25,65 @@ from gowal21uncovering.utils import WATrainer
 
 # Setup
 
-parse = parser_train()
-parse.add_argument('--tau', type=float, default=0.995, help='Weight averaging decay.')
-args = parse.parse_args()
-assert args.data in SEMISUP_DATASETS, f'Only data in {SEMISUP_DATASETS} is supported!'
+# parse = parser_train()
+# parse.add_argument("--tau", type=float, default=0.995, help="Weight averaging decay.")
+# args = parse.parse_args()
+#
+args_dict = {
+    "augment": "base",
+    "batch_size": 512,
+    "batch_size_validation": 512,
+    "data_dir": "dataset-data",
+    "log_dir": "tiny_edm_models",
+    "data": "tiny-imagenets",
+    "desc": "WRN28-10Swish_tiny_lr0p2_CrossEntLoss_epoch400_bs512_fraction0p8_ls0p1_seed0_attackstep2_edm",
+    "model": "wrn-28-10-swish",
+    "normalize": False,
+    "pretrained_file": None,
+    "num_adv_epochs": 400,
+    "adv_eval_freq": 25,
+    "beta": 8.0,
+    "lr": 0.2,
+    "weight_decay": 0.0005,
+    "scheduler": "cosinew",
+    "nesterov": True,
+    "clip_grad": None,
+    "attack": "linf-pgd",
+    "attack_eps": 0.03137254901960784,
+    "attack_step": 0.00784313725490196,
+    "attack_iter": 10,
+    "keep_clean": False,
+    "debug": False,
+    "mart": False,
+    "unsup_fraction": 0.8,
+    "aux_data_filename": "./edm_data/tiny/1m.npz",
+    "seed": 0,
+    "consistency": False,
+    "cons_lambda": 1.0,
+    "cons_tem": 0.5,
+    "resume_path": "",
+    "LSE": False,
+    "ls": 0.1,
+    "clip_value": 0,
+    "CutMix": False,
+    "fast_at": False,
+    "traversal": False,
+    "tau": 0.995,
+}
+args = argparse.Namespace(**args_dict)
+
+assert args.data in SEMISUP_DATASETS, f"Only data in {SEMISUP_DATASETS} is supported!"
 
 
 DATA_DIR = os.path.join(args.data_dir, args.data)
 LOG_DIR = os.path.join(args.log_dir, args.desc)
-WEIGHTS = os.path.join(LOG_DIR, 'weights-best.pt')
+WEIGHTS = os.path.join(LOG_DIR, "weights-best.pt")
 if os.path.exists(LOG_DIR):
     shutil.rmtree(LOG_DIR)
 os.makedirs(LOG_DIR)
-logger = Logger(os.path.join(LOG_DIR, 'log-train.log'))
+logger = Logger(os.path.join(LOG_DIR, "log-train.log"))
 
-with open(os.path.join(LOG_DIR, 'args.txt'), 'w') as f:
+with open(os.path.join(LOG_DIR, "args.txt"), "w") as f:
     json.dump(args.__dict__, f, indent=4)
 
 
@@ -47,8 +91,8 @@ info = get_data_info(DATA_DIR)
 BATCH_SIZE = args.batch_size
 BATCH_SIZE_VALIDATION = args.batch_size_validation
 NUM_ADV_EPOCHS = args.num_adv_epochs
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-logger.log('Using device: {}'.format(device))
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+logger.log("Using device: {}".format(device))
 if args.debug:
     NUM_ADV_EPOCHS = 1
 
@@ -59,9 +103,23 @@ torch.backends.cudnn.benchmark = True
 # Load data
 
 seed(args.seed)
-train_dataset, test_dataset, eval_dataset, train_dataloader, test_dataloader, eval_dataloader = load_data(
-    DATA_DIR, BATCH_SIZE, BATCH_SIZE_VALIDATION, use_augmentation=args.augment, use_consistency=args.consistency, shuffle_train=True, 
-    aux_data_filename=args.aux_data_filename, unsup_fraction=args.unsup_fraction, validation=True
+(
+    train_dataset,
+    test_dataset,
+    eval_dataset,
+    train_dataloader,
+    test_dataloader,
+    eval_dataloader,
+) = load_data(
+    DATA_DIR,
+    BATCH_SIZE,
+    BATCH_SIZE_VALIDATION,
+    use_augmentation=args.augment,
+    use_consistency=args.consistency,
+    shuffle_train=True,
+    aux_data_filename=args.aux_data_filename,
+    unsup_fraction=args.unsup_fraction,
+    validation=True,
 )
 del train_dataset, test_dataset, eval_dataset
 
@@ -70,7 +128,7 @@ del train_dataset, test_dataset, eval_dataset
 
 seed(args.seed)
 if args.tau:
-    print ('Using WA.')
+    print("Using WA.")
     trainer = WATrainer(info, args)
 else:
     trainer = Trainer(info, args)
@@ -78,71 +136,102 @@ last_lr = args.lr
 
 
 if NUM_ADV_EPOCHS > 0:
-    logger.log('\n\n')
+    logger.log("\n\n")
     metrics = pd.DataFrame()
-    logger.log('Standard Accuracy-\tTest: {:2f}%.'.format(trainer.eval(test_dataloader)*100))
-    
+    logger.log(
+        "Standard Accuracy-\tTest: {:2f}%.".format(trainer.eval(test_dataloader) * 100)
+    )
+
     old_score = [0.0, 0.0]
-    logger.log('RST Adversarial training for {} epochs'.format(NUM_ADV_EPOCHS))
+    logger.log("RST Adversarial training for {} epochs".format(NUM_ADV_EPOCHS))
     trainer.init_optimizer(args.num_adv_epochs)
-    test_adv_acc = 0.0    
+    test_adv_acc = 0.0
 
 if args.resume_path:
-    start_epoch = trainer.load_model_resume(os.path.join(args.resume_path, 'state-last.pt')) + 1
-    logger.log(f'Resuming at epoch {start_epoch}')
+    start_epoch = (
+        trainer.load_model_resume(os.path.join(args.resume_path, "state-last.pt")) + 1
+    )
+    logger.log(f"Resuming at epoch {start_epoch}")
 else:
     start_epoch = 1
 
-for epoch in range(start_epoch, NUM_ADV_EPOCHS+1):
+for epoch in range(start_epoch, NUM_ADV_EPOCHS + 1):
     start = time.time()
-    logger.log('======= Epoch {} ======='.format(epoch))
-    
+    logger.log("======= Epoch {} =======".format(epoch))
+
     if args.scheduler:
         last_lr = trainer.scheduler.get_last_lr()[0]
-    
-    res = trainer.train(train_dataloader, epoch=epoch, adversarial=True)
+
+    res = trainer.train(train_dataloader, epoch=epoch, adversarial=False)
     test_acc = trainer.eval(test_dataloader)
-    
-    logger.log('Loss: {:.4f}.\tLR: {:.4f}'.format(res['loss'], last_lr))
-    if 'clean_acc' in res:
-        logger.log('Standard Accuracy-\tTrain: {:.2f}%.\tTest: {:.2f}%.'.format(res['clean_acc']*100, test_acc*100))
+
+    logger.log("Loss: {:.4f}.\tLR: {:.4f}".format(res["loss"], last_lr))
+    if "clean_acc" in res:
+        logger.log(
+            "Standard Accuracy-\tTrain: {:.2f}%.\tTest: {:.2f}%.".format(
+                res["clean_acc"] * 100, test_acc * 100
+            )
+        )
     else:
-        logger.log('Standard Accuracy-\tTest: {:.2f}%.'.format(test_acc*100))
-    epoch_metrics = {'train_'+k: v for k, v in res.items()}
-    epoch_metrics.update({'epoch': epoch, 'lr': last_lr, 'test_clean_acc': test_acc, 'test_adversarial_acc': ''})
-    
-    if epoch % args.adv_eval_freq == 0 or epoch == NUM_ADV_EPOCHS:        
+        logger.log("Standard Accuracy-\tTest: {:.2f}%.".format(test_acc * 100))
+    epoch_metrics = {"train_" + k: v for k, v in res.items()}
+    epoch_metrics.update(
+        {
+            "epoch": epoch,
+            "lr": last_lr,
+            "test_clean_acc": test_acc,
+            "test_adversarial_acc": "",
+        }
+    )
+
+    if epoch % args.adv_eval_freq == 0 or epoch == NUM_ADV_EPOCHS:
         test_adv_acc = trainer.eval(test_dataloader, adversarial=True)
-        logger.log('Adversarial Accuracy-\tTrain: {:.2f}%.\tTest: {:.2f}%.'.format(res['adversarial_acc']*100, 
-                                                                                   test_adv_acc*100))
-        epoch_metrics.update({'test_adversarial_acc': test_adv_acc})
+        logger.log(
+            "Adversarial Accuracy-\tTrain: {:.2f}%.\tTest: {:.2f}%.".format(
+                res["adversarial_acc"] * 100, test_adv_acc * 100
+            )
+        )
+        epoch_metrics.update({"test_adversarial_acc": test_adv_acc})
     else:
-        logger.log('Adversarial Accuracy-\tTrain: {:.2f}%.'.format(res['adversarial_acc']*100))
-    eval_adv_acc = trainer.eval(eval_dataloader, adversarial=True)
-    logger.log('Adversarial Accuracy-\tEval: {:.2f}%.'.format(eval_adv_acc*100))
-    epoch_metrics['eval_adversarial_acc'] = eval_adv_acc
-    
+        logger.log(
+            "Adversarial Accuracy-\tTrain: {:.2f}%.".format(
+                res["adversarial_acc"] * 100
+            )
+        )
+    eval_adv_acc = trainer.eval(eval_dataloader, adversarial=False)
+    logger.log("Adversarial Accuracy-\tEval: {:.2f}%.".format(eval_adv_acc * 100))
+    epoch_metrics["eval_adversarial_acc"] = eval_adv_acc
+
     if eval_adv_acc >= old_score[1]:
         old_score[0], old_score[1] = test_acc, eval_adv_acc
         trainer.save_model(WEIGHTS)
     # trainer.save_model(os.path.join(LOG_DIR, 'weights-last.pt'))
     if epoch % 10 == 0:
-        trainer.save_model_resume(os.path.join(LOG_DIR, 'state-last.pt'), epoch) 
+        trainer.save_model_resume(os.path.join(LOG_DIR, "state-last.pt"), epoch)
     if epoch % 400 == 0:
-        shutil.copyfile(WEIGHTS, os.path.join(LOG_DIR, f'weights-best-epoch{str(epoch)}.pt'))
+        shutil.copyfile(
+            WEIGHTS, os.path.join(LOG_DIR, f"weights-best-epoch{str(epoch)}.pt")
+        )
 
-    logger.log('Time taken: {}'.format(format_time(time.time()-start)))
+    logger.log("Time taken: {}".format(format_time(time.time() - start)))
     metrics = metrics.append(pd.DataFrame(epoch_metrics, index=[0]), ignore_index=True)
-    metrics.to_csv(os.path.join(LOG_DIR, 'stats_adv.csv'), index=False)
+    metrics.to_csv(os.path.join(LOG_DIR, "stats_adv.csv"), index=False)
 
-    
-    
+
 # Record metrics
 
-train_acc = res['clean_acc'] if 'clean_acc' in res else trainer.eval(train_dataloader)
-logger.log('\nTraining completed.')
-logger.log('Standard Accuracy-\tTrain: {:.2f}%.\tTest: {:.2f}%.'.format(train_acc*100, old_score[0]*100))
+train_acc = res["clean_acc"] if "clean_acc" in res else trainer.eval(train_dataloader)
+logger.log("\nTraining completed.")
+logger.log(
+    "Standard Accuracy-\tTrain: {:.2f}%.\tTest: {:.2f}%.".format(
+        train_acc * 100, old_score[0] * 100
+    )
+)
 if NUM_ADV_EPOCHS > 0:
-    logger.log('Adversarial Accuracy-\tTrain: {:.2f}%.\tEval: {:.2f}%.'.format(res['adversarial_acc']*100, old_score[1]*100)) 
+    logger.log(
+        "Adversarial Accuracy-\tTrain: {:.2f}%.\tEval: {:.2f}%.".format(
+            res["adversarial_acc"] * 100, old_score[1] * 100
+        )
+    )
 
-logger.log('Script Completed.')
+logger.log("Script Completed.")
